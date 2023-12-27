74128e92033824512ee10565fbe93510dc1e72b1
dearfad <dearfad@sina.com>
2023-12-27 22:06:14 +0800

    update

--- breastvsp.py
+++ breastvsp.py
@@ -1,21 +1,47 @@
 from libs.models.mscope import get_model
-from libs.asr.speechrecognition import listen
-from libs.tts.edge import say
+# from libs.asr.speechrecognition import listen
+# from libs.tts.edge import say
+import streamlit as st
+
+st.title("Echo Bot")
+
+tokenizer, model = get_model()
 
 
-def main():
+# if "openai_model" not in st.session_state:
+#     st.session_state["openai_model"] = "gpt-3.5-turbo"
 
-    tokenizer, model = get_model('ZhipuAI/chatglm3-6b')
-    model = model.quantize(4).cuda().eval()
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-    history = []
-    while True:
-        message = listen()
-        print(message)
-        response, history = model.chat(tokenizer, message, history=history)
-        print(response)
-        say(response)
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    print('1st prompt', list(st.session_state.messages))
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    print('2nd message: ', list(st.session_state.messages))
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        for response, history in model.stream_chat(tokenizer, prompt, history=list(st.session_state.messages)):
+            message_placeholder.markdown(response)
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    print('3: ', list(st.session_state.messages))
+    st.session_state.messages.append({"role": "assistant", "content": response})
+    print(list(st.session_state.messages))
 
 
-if __name__ == '__main__':
-    main()
+
+# def main():
+
+#     tokenizer, model = get_model()
+
+#     history = []
+#     while True:
+#         message = listen()
+#         print(message)
+#         response, history = model.chat(tokenizer, message, history=history)
+#         print(response)
+#         say(response)
--- libs/models/mscope.py
+++ libs/models/mscope.py
@@ -1,20 +1,21 @@
 import os
 from modelscope.hub.snapshot_download import snapshot_download
 from modelscope import AutoTokenizer, AutoModel
+import streamlit as st
 
 # MODEL_NAME_DICT = {
 #     'modelscope': 'ZhipuAI/chatglm3-6b',
 #     'huggingface': 'THUDM/chatglm3-6b',
 # }
 
-
-def get_model(model_id):
+@st.cache_resource
+def get_model(model_id='ZhipuAI/chatglm3-6b'):
     MODELSCOPE_CACHE_PATH = os.environ.get('MODELSCOPE_CACHE')
     print('MODELSCOPE_CACHE_PATH: ', MODELSCOPE_CACHE_PATH)
     MODEL_PATH = snapshot_download(model_id)
     print('MODEL_PATH: ', MODEL_PATH)
     tokenizer = AutoTokenizer.from_pretrained(model_id, trust_remote_code=True)
-    model = AutoModel.from_pretrained(model_id, trust_remote_code=True)
+    model = AutoModel.from_pretrained(model_id, trust_remote_code=True).quantize(8).cuda().eval()
     return tokenizer, model
 
 
