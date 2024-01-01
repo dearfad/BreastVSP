fb010870173584b56144869fdd7fcae9dc311669
dearfad <dearfad@sina.com>
2024-01-01 21:37:03 +0800

    update

--- webui_streamlit.py
+++ webui_streamlit.py
@@ -1,7 +1,42 @@
+import os
 import streamlit as st
-from libs.models.mscope import get_model
 import speech_recognition as sr
-from libs.tts.edge import tts_say
+
+import edge_tts
+import asyncio
+from playsound import playsound
+from pyaudio import PyAudio
+
+from modelscope.hub.snapshot_download import snapshot_download
+from modelscope import AutoTokenizer, AutoModel
+
+# LLM MODEL
+@st.cache_resource(show_spinner=False)
+def get_model(model_id='ZhipuAI/chatglm3-6b'):
+    # MODELSCOPE_CACHE_PATH = os.environ.get('MODELSCOPE_CACHE')
+    # print('MODELSCOPE_CACHE_PATH: ', MODELSCOPE_CACHE_PATH)
+    # MODEL_PATH = snapshot_download(model_id)
+    # print('MODEL_PATH: ', MODEL_PATH)
+    tokenizer = AutoTokenizer.from_pretrained(model_id, trust_remote_code=True)
+    model = AutoModel.from_pretrained(
+        model_id, trust_remote_code=True).quantize(8).cuda().eval()
+    return tokenizer, model
+
+# TTS
+def tts_say(message, voice):
+    asyncio.run(speak(message, voice))
+
+async def speak(message, voice):
+    text = message
+    output = './static/lastspeech.mp3'
+    rate = '-4%'
+    volume = '+0%'
+    tts = edge_tts.Communicate(
+        text=text, voice=voice, rate=rate, volume=volume)
+    await tts.save(output)
+    playsound(output)
+    return
+
 
 # LLM
 # ModelScope ZhipuAI/chatglm3-6b
@@ -41,7 +76,6 @@ for message in st.session_state.messages:
         st.write(message["content"])
 
 
-
 prompt = st.chat_input('')
 
 
@@ -63,7 +97,9 @@ def chat():
             {'role': 'assistant', 'content': "没有使用大语言模型"})
 
     if tts_toggle:
-        tts_say(response, voice='zh-CN-XiaoxiaoNeural')
+        tts_say(response, 'zh-CN-XiaoxiaoNeural')
+        # p = Process(target=tts_say, args=(response, 'zh-CN-XiaoxiaoNeural'))
+        # p.start()
 
 
 if prompt:
@@ -71,18 +107,15 @@ if prompt:
 
 
 if asr_toggle:
-    temp = True
-    while temp:
-        r = sr.Recognizer()
-        with sr.Microphone() as source:
-            mic_status_placeholder = st.empty()
-            mic_status_placeholder.write('请说...')
-            audio = r.listen(source, timeout=None, phrase_time_limit=10)
-            mic_status_placeholder.write('识别中...')
-            asr_message = r.recognize_whisper(audio, model="small", language="chinese")
-            mic_status_placeholder.empty()
-            if asr_message:
-                prompt = asr_message
-                chat()
-            if asr_message == '退出':
-                temp = False
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        mic_status_placeholder = st.empty()
+        mic_status_placeholder.write('请说...')
+        audio = r.listen(source, timeout=None, phrase_time_limit=10)
+        mic_status_placeholder.write('识别中...')
+        asr_message = r.recognize_whisper(
+            audio, model="small", language="chinese")
+        mic_status_placeholder.empty()
+        if asr_message:
+            prompt = asr_message
+            chat()
