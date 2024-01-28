01bc46ae719cf61feff73d142232a3a2833252ec
dearfad <dearfad@gmail.com>
2024-01-28 13:46:06 +0800

    update

--- breastvsp.py
+++ breastvsp.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="BreastVSP",
+    page_icon="👩",
+    layout="centered",
+    initial_sidebar_state="auto",
+    menu_items=None
+)
+
 st.title('Breast VSP')
